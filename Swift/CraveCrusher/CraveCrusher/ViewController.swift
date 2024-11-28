563f0e5cc09c1ed2b123f4ca41e81c54d944c6dc
michae-lzhou <ziyang.zhou@gmail.com>
2024-11-28 15:09:33 -0500

    Added initial Swift prototype (untested) of the home screen to the repo

---
+++ Swift/CraveCrusher/CraveCrusher/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CraveCrusher
+//
+//  Created by Michael Zhou on 11/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var gameTitle: UILabel!
+    
+    @IBAction func playButton(_ sender: Any) {
+        print("user clicked play")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift/CraveCrusher/CraveCrusherTests/CraveCrusherTests.swift
@@ -0,0 +1,17 @@
+//
+//  CraveCrusherTests.swift
+//  CraveCrusherTests
+//
+//  Created by Michael Zhou on 11/27/24.
+//
+
+import Testing
+@testable import CraveCrusher
+
+struct CraveCrusherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
