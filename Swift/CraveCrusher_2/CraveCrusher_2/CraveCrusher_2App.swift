e85ece604333074e5629c594d4fcd58a8203e6e1
michae-lzhou <ziyang.zhou@gmail.com>
2025-01-30 23:12:42 -0500

    New copy of project without git repo nested

---
+++ Swift/CraveCrusher_2/CraveCrusher_2/CraveCrusher_2App.swift
@@ -0,0 +1,17 @@
+//
+//  CraveCrusher_2App.swift
+//  CraveCrusher_2
+//
+//  Created by Michael Zhou on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct CraveCrusher_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift/CraveCrusher_2/CraveCrusher_2/MenuView.swift
@@ -0,0 +1,24 @@
+//
+//  MenuView.swift
+//  CraveCrusher_1
+//
+//  Created by Michael Zhou on 1/30/25.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Menu Screen")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MenuView()
+}
