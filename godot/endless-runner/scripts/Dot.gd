ef694eb011e73634293293f4cf609ca8b254d7b1
michaelhu22 <michaelhu218@gmail.com>
2025-05-23 17:16:38 -0400

    cleanup

--- godot/endless-runner/scripts/Dot.gd
+++
@@ -1,8 +0,0 @@
-# Dot.gd
-extends Node2D
-
-@export var radius: float = 4.0
-@export var color: Color = Color.WHITE
-
-func _draw():
-	draw_circle(Vector2.ZERO, radius, color)
--- godot/endless-runner/scripts/GameManager.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-var score = 0
-
-func update_score():
-	$ScoreLabel.text = "Score: " + str(score)
